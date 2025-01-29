5803beaf98b95be3c2fc4b9c4aec1c3bc7d106c7
Maxim Khanukaev <Andreymaxim387@gmail.com>
2025-01-29 22:26:42 +0300

    Добавил комментарии к коду

--- src/pages/CountryPage.tsx
+++ src/pages/CountryPage.tsx
@@ -1,67 +1,74 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { Button } from '../components/ui/button'
-import {
-	Card,
+// Импортируем необходимые библиотеки и файлы
+import axios from 'axios' // Библиотека для выполнения HTTP-запросов
+import { useEffect, useState } from 'react' // Хуки React для управления состоянием и жизненным циклом компонента
+import { useNavigate, useParams } from 'react-router-dom' // Хуки для навигации и получения параметров маршрута
+import { Button } from '../components/ui/button' // Компонент кнопки из локального файла
+import { Card, CardFooter, CardHeader, CardTitle } from '../components/ui/card' // Компоненты карточки из другого файла проекта
+import { esimProvider } from '../store/types' // Типы данных для провайдеров eSIM
+import { useTelegram } from '../hooks/useTelegram' // Кастомный хук для работы с Telegram Mini Apps
 
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from '../components/ui/card'
-import { esimProvider } from '../store/types'
-import { useTelegram } from '../hooks/useTelegram'
-
+// Компонент (можно использовать многократно в разных местах)
 const CountryPage = () => {
-	const { tg, BackButton } = useTelegram()
-	const navigate = useNavigate()
-	const [data, setData] = useState<esimProvider>()
-	const { id } = useParams()
+	const { tg, BackButton } = useTelegram() // Получаем объект Telegram и кнопку "Назад" из кастомного хука
+	const navigate = useNavigate() // Хук для навигации между страницами
+	const [data, setData] = useState<esimProvider>() // Состояние для хранения данных о провайдере eSIM
+	const { id } = useParams() // Получаем параметр из ссылки страницы (id страны)
 
 	useEffect(() => {
-		BackButton.show()
+		BackButton.show() // Показываем кнопку "Назад" в интерфейсе Telegram
 
+		// Добавляем обработчик события для кнопки "Назад"
 		tg.onEvent('backButtonClicked', () => {
-      tg.BackButton.hide(); 
-      window.history.back(); 
-			navigate('/');
-    });
+			tg.BackButton.hide() // Скрываем кнопку "Назад"
+			window.history.back() // Возвращаемся на предыдущую страницу в истории браузера
+			navigate('/') // Перенаправляем пользователя на главную страницу
+		})
 
+		// Если параметр id существует, выполняем запрос на сервер для получения данных
 		if (id) {
 			axios
-				.get(`https://66f4456877b5e8897098fcda.mockapi.io/api/${id}`)
-				.then(res => setData(res.data))
-				.catch(err => console.log(err))
+				.get(`https://66f4456877b5e8897098fcda.mockapi.io/api/${id}`) // Выполняем GET-запрос
+				.then(res => setData(res.data)) // Устанавливаем полученные данные в состояние
+				.catch(err => console.log(err)) // Обрабатываем ошибку, если запрос не удался
 		}
 
+		// Очистка при размонтировании компонента
 		return () => {
-      tg.BackButton.hide();
-      tg.offEvent('backButtonClicked'); // Removes the event listener
-    };
-	}, [])
-	
+			tg.BackButton.hide() // Скрываем кнопку "Назад"
+			tg.offEvent('backButtonClicked') // Удаляем обработчик события для кнопки "Назад"
+		}
+	}, []) // Пустой массив зависимостей означает, что эффект выполнится только при монтировании и размонтировании компонента
+
+	// Отрисовываем компонент
 	return (
 		<div className='w-full flex flex-col justify-center'>
-			<div className="w-350px">
-				<div className="flex flex-col space-y-1.5 p-6">
-					<div className="flex gap-3 items-center text-2xl font-semibold leading-none tracking-tight">
-						<img src={data?.flag} className='w-8'/> 
-						{`${data?.country}`}
+			<div className='w-350px'>
+				<div className='flex flex-col space-y-1.5 p-6'>
+					<div className='flex gap-3 items-center text-2xl font-semibold leading-none tracking-tight'>
+						<img src={data?.flag} className='w-8' />{' '}
+						{/* Отображаем флаг страны */}
+						{`${data?.country}`} {/* Отображаем название страны */}
 					</div>
-					<div className="text-sm text-muted-foreground">
-						Выберите оператора
+					<div className='text-sm text-muted-foreground'>
+						Выберите оператора {/* Подсказка для пользователя */}
 					</div>
-					</div>
-				<div className="flex flex-col gap-4">
+				</div>
+				<div className='flex flex-col gap-4'>
+					{/* Отображаем список операторов */}
 					{data?.operators.map(operator => (
 						<Card>
 							<CardHeader>
-								<CardTitle className='flex'> 
-									<img src={operator.logo} alt="" className='w-10 h-10'/>
-									{operator.name}</CardTitle>
+								<CardTitle className='flex'>
+									<img src={operator.logo} alt='' className='w-10 h-10' />{' '}
+									{/* Логотип оператора */}
+									{operator.name} {/* Название оператора */}
+								</CardTitle>
 							</CardHeader>
 							<CardFooter>
-								<Button><a href={operator.url}>Купить</a></Button>
+								<Button>
+									<a href={operator.url}>Купить</a>
+								</Button>{' '}
+								{/* Кнопка для перехода на страницу покупки */}
 							</CardFooter>
 						</Card>
 					))}
@@ -71,4 +78,4 @@ const CountryPage = () => {
 	)
 }
 
-export default CountryPage
+export default CountryPage // Экспортируем компонент для использования в других частях приложения
--- src/pages/ListOfCountries.tsx
+++ src/pages/ListOfCountries.tsx
@@ -1,41 +1,58 @@
-import { Link } from 'react-router-dom'
-import { Button } from '../components/ui/button'
-import axios from 'axios'
-import {
-	Card,
-	CardHeader,
-	CardTitle,
-} from '../components/ui/card'
-import Search from '../components/search'
-import { useEffect, useState } from 'react'
-import useSearchStore from '../store/search'
-import { esimProvider } from '../store/types'
+// Импорт необходимых компонентов и библиотек
+import { Link } from 'react-router-dom' // Компонент для создания ссылок в React Router
+import { Button } from '../components/ui/button' // Кастомный компонент кнопки
+import axios from 'axios' // Библиотека для выполнения HTTP-запросов
+import { Card, CardHeader, CardTitle } from '../components/ui/card' // Компоненты для создания карточек
+import Search from '../components/search' // Компонент поиска
+import { useEffect, useState } from 'react' // Хуки для управления состоянием и жизненным циклом компонента
+import useSearchStore from '../store/search' // Кастомный хук для управления состоянием поиска
+import { esimProvider } from '../store/types' // Тип данных для провайдеров eSIM
 
+// Основной компонент ListOfCountries
 const ListOfCountries = () => {
+	// Состояние для хранения данных о странах и провайдерах eSIM
 	const [data, setData] = useState<esimProvider[]>([])
+
+	// Получение текущего значения поиска из хранилища
 	const { search } = useSearchStore()
 
+	// Эффект для загрузки данных при монтировании компонента
 	useEffect(() => {
 		axios
-			.get('https://66f4456877b5e8897098fcda.mockapi.io/api')
-			.then(res => setData(res.data))
-			.catch(err => console.log(err))
-	}, [])
+			.get('https://66f4456877b5e8897098fcda.mockapi.io/api') // Запрос данных с API
+			.then(res => setData(res.data)) // Установка данных в состояние
+			.catch(err => console.log(err)) // Обработка ошибок
+	}, []) // Пустой массив зависимостей означает, что эффект выполнится только один раз
 
+	// Рендер компонента
 	return (
 		<div className='w-full flex flex-col justify-center items-center gap-5'>
+			{/* Компонент поиска */}
 			<Search />
+
+			{/* Контейнер для отображения списка стран */}
 			<div className='w-full flex flex-col items-center gap-3'>
+				{/* Фильтрация данных по значению поиска и отображение карточек */}
 				{data
-					?.filter(item => item.search.includes(search.toLowerCase()))
+					?.filter(item => item.search.includes(search.toLowerCase())) // Фильтрация данных
 					.map(item => (
+						// Карточка для каждой страны
 						<Card className='w-full' key={item.id}>
 							<CardHeader className='p-4'>
 								<CardTitle className='flex justify-between items-center'>
+									{/* Блок с флагом и названием страны */}
 									<div className='flex items-center'>
-										<div className='mr-2 text-4xl'><img src={item.flag} className='w-6'/></div>
+										<div className='mr-2 text-4xl'>
+											<img
+												src={item.flag}
+												className='w-6'
+												alt={`Флаг ${item.country}`}
+											/>
+										</div>
 										<div className='text-sm'>{item.country}</div>
 									</div>
+
+									{/* Кнопка для перехода на страницу покупки */}
 									<Button>
 										<Link to={`/${item.id}`}>Купить</Link>
 									</Button>
@@ -48,4 +65,5 @@ const ListOfCountries = () => {
 	)
 }
 
+// Экспорт компонента по умолчанию
 export default ListOfCountries
